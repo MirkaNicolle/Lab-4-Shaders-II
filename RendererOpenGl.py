5595969145979288358946f8257a59411af1e08e
Mirka <mirkanicolle@gmail.com>
2021-11-15 20:09:31 -0600

    archivos

---
+++ RendererOpenGl.py
@@ -0,0 +1,8 @@
+'''
+Source: https://github.com/churly92/RendererOpenGL
+
+Mirka Monzon 18139
+Lab 4: Shaders II
+
+'''
+
---
+++ gl.py
@@ -0,0 +1,7 @@
+'''
+Source: https://github.com/churly92/RendererOpenGL
+
+Mirka Monzon 18139
+Lab 4: Shaders II
+
+'''
